f03759e92cbaa597e7271459764ca09eb883b93c
Hugo Gabriel <hugogabrieltjj@gmail.com>
2021-10-26 20:29:13 -0300

    project done

---
+++ src/componentes/Footer/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './estilo.css'
+
+function Footer (){
+  return (
+  <div className="container">
+  <div className="title">
+    Mensagem rodapé 
+    </div>
+  </div>
+      )
+}
+export default Footer;
